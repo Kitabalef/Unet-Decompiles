eb2e08abdb8b0ab7c2c33db9ebfaeab5fff53756
Joe Brown <jaybrizle@gmail.com>
2015-06-17 06:12:48 -0400

    added comments to NetProximityChecker

--- NetworkProximityChecker.cs
+++ NetworkProximityChecker.cs
@@ -38,7 +38,10 @@ namespace UnityEngine.Networking
 		///   <para>Flag to force this object to be hidden for players.</para>
 		/// </summary>
 		public bool forceHidden;
-		private float visUpdateTime;
+
+        private float visUpdateTime; // counter used in Update to rebuild the observers of this NetworkIdentity object
+
+
 		private void Update()
 		{
 			if (!NetworkServer.active)
@@ -51,27 +54,41 @@ namespace UnityEngine.Networking
 				this.visUpdateTime = Time.get_time();
 			}
 		}
+
+
+        // NetBehaviour override
 		public override bool OnCheckObserver(NetworkConnection newObserver)
 		{
 			if (this.forceHidden)
 			{
 				return false;
 			}
+
 			PlayerController playerController = newObserver.playerControllers[0];
 			Vector3 position = playerController.unetView.get_gameObject().get_transform().get_position();
 			return (position - base.get_transform().get_position()).get_magnitude() < (float)this.visRange;
 		}
+
+
+        // This method is the NetBehavior callback for the NetworkIdentity RebuildObservers method
+        // every NetBehaviour script receives a callback here when RebuildObservers is called
+        // it is passed the current list of observers for this object.. "observers" is an available NetworkIdentity property
 		public override bool OnRebuildObservers(HashSet<NetworkConnection> observers, bool initial)
 		{
 			if (this.forceHidden)
 			{
+                // get this gameobjects NetworkIdentity to test for a connection to a client
+                // this must test to see of this is attached to a player object here then even though the docs dont specifically say that..
+                // connectionToClient is only valid for player objects on the server
 				NetworkIdentity component = base.GetComponent<NetworkIdentity>();
 				if (component.connectionToClient != null)
 				{
 					observers.Add(component.connectionToClient);
 				}
+
 				return true;
 			}
+
 			NetworkProximityChecker.CheckMethod checkMethod = this.checkMethod;
 			if (checkMethod == NetworkProximityChecker.CheckMethod.Physics3D)
 			{
@@ -81,6 +98,8 @@ namespace UnityEngine.Networking
 				{
 					Collider collider = array2[i];
 					NetworkIdentity component2 = collider.GetComponent<NetworkIdentity>();
+
+                    // check if the collided object is a player object on the server with a valid connection to client
 					if (component2 != null && component2.connectionToClient != null)
 					{
 						observers.Add(component2.connectionToClient);
@@ -88,10 +107,13 @@ namespace UnityEngine.Networking
 				}
 				return true;
 			}
+
+            // at this point in the method the last option is 2d detection, check to make sure it has been selected then
 			if (checkMethod != NetworkProximityChecker.CheckMethod.Physics2D)
 			{
 				return false;
 			}
+
 			Collider2D[] array3 = Physics2D.OverlapCircleAll(base.get_transform().get_position(), (float)this.visRange);
 			Collider2D[] array4 = array3;
 			for (int j = 0; j < array4.Length; j++)
@@ -105,10 +127,16 @@ namespace UnityEngine.Networking
 			}
 			return true;
 		}
+
+
+        // this method i used to manage the local/host client situation. The host shares the scene with the server, so "non-visible"
+        // objects to this local player can only be hidden/unrendered in the scene for them
 		public override void OnSetLocalVisibility(bool vis)
 		{
 			NetworkProximityChecker.SetVis(base.get_gameObject(), vis);
 		}
+
+        // disable rendering of gameobjects the host should not see. He does not use network bandwidth anyway, so just non rendering is needed
 		private static void SetVis(GameObject go, bool vis)
 		{
 			Renderer[] components = go.GetComponents<Renderer>();
